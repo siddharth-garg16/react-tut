6f92e4c412ed0578ca1cdbd590be369fbe4da03d
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-12-22 16:52:09 +0530

    file structure and components

---
+++ demo1/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css"
+import SubComponent from "./components/SubComponent.js"
+
+function App(){
+    return(
+        // <div>
+        //     <h3 className="head">First Component</h3>
+        //     <p>This is a p tag.</p>
+        // </div>);
+        <SubComponent />);
+}
+
+export default App;
---
+++ demo1/src/components/SubComponent.js
@@ -0,0 +1,11 @@
+import "./SubComponent.css"
+function SubComponent(){
+    return (
+        <div>
+            <h3 id="sub-head">SubComponent</h3>
+            <p>SubComponent text.</p>
+        </div>
+    )
+}
+
+export default SubComponent;
